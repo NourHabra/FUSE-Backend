f9ce373144d4c490d0c947179785b85da247446f
XxEngAbdullahxX <119155861+XxEngAbdullahxX@users.noreply.github.com>
2024-05-26 13:19:59 +0300

    role validation

--- middleware/authRole.js
+++ middleware/authRole.js
@@ -1,17 +1,34 @@
-const isAdmin = (req, res, next) => {
-    if(req.user.role === "admin"){
+const isCustomer = (req, res, next) => {
+    if(req.user.role === "Customer"){
         next();
     } else {
         return res.status(401).json({ message: 'Unauthorized only admin' });
     }
 }
 
-const isUser = (req, res, next) => {
-    if(req.user.role === "user"){
+const isMerchant = (req, res, next) => {
+    if(req.user.role === "Merchant"){
         next();
     } else {
         return res.status(401).json({ message: 'Unauthorized only admin' });
     }
 }
 
-module.exports = {isAdmin, isUser};
+const isVendor = (req, res, next) => {
+    if(req.user.role === "Vendor"){
+        next();
+    } else {
+        return res.status(401).json({ message: 'Unauthorized only admin' });
+    }
+}
+
+const isEmployee = (req, res, next) => {
+    if(req.user.role === "Employee"){
+        next();
+    } else {
+        return res.status(401).json({ message: 'Unauthorized only admin' });
+    }
+}
+
+
+module.exports = { isCustomer, isEmployee, isMerchant, isVendor};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,11 +3,11 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 
 const authenticateJWT = require('../middleware/authMiddleware');
-const { isAdmin } = require('../middleware/authRole');
+const { isCustomer } = require('../middleware/authRole');
 
 //router.get('/create', userController.create);
 
-router.get('/', userController.index);
+router.get('/', authenticateJWT, isCustomer, userController.index);
 router.get('/:id', userController.show);
 router.put('/:id', userController.update);
 router.delete('/:id', userController.destroy);
