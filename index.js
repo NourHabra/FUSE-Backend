1d4023654f7946c8b98c5693e1c948902202b430
NourHabra <NourHBSU@gmail.com>
2024-05-26 12:49:44 +0300

    Updated port to be environment or 3000

--- index.js
+++ index.js
@@ -7,10 +7,16 @@ const accountRoutes = require("./routes/accountRoutes");
 const cardRoutes = require("./routes/cardRoutes");
 const transactionRoutes = require("./routes/transactionRoutes");
 
+PORT = process.env.PORT | 3000;
+
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(express.json());
+
+app.get("/", async (req, res) => {
+	res.json({ msg: "Hello World, I am alive!" });
+});
 
 app.use("/user", userRoutes);
 app.use("/auth", authRouter);
@@ -20,7 +26,6 @@ app.use("/account", accountRoutes);
 app.use("/card", cardRoutes);
 app.use("/transaction", transactionRoutes);
 
-app.listen(3000, () => {
-    console.log('Server listening on port 3000');
+app.listen(PORT, () => {
+	console.log("Server listening on port 3000");
 });
-
