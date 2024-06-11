5f5a9f6d10e4423958be6d67a677415ebc5daa14
XxEngAbdullahxX <119155861+XxEngAbdullahxX@users.noreply.github.com>
2024-06-11 15:39:05 +0300

    stoping helmet

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const app = express();
 
 app.use(express.static("public"));
 app.use(express.json());
-app.use(helmet());
+//app.use(helmet());
 
 app.get("/", async (req, res) => {
 	res.json({ msg: "Hello World, I am alive!" });
