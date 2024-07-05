60c9dd98e4c1efbd26d8a0b81663726681d5ed1a
XxEngAbdullahxX <119155861+XxEngAbdullahxX@users.noreply.github.com>
2024-07-05 17:42:10 +0300

    l

--- controllers/logController.js
+++ controllers/logController.js
@@ -2,17 +2,16 @@
 
 async function logServer(req, res) {
 	const requestBody = { ...req.body };
+	if (requestBody.jwt) {
+		delete requestBody.jwt;
+	}
 	if (req.user) {
-    const requestBody = { ...req.body };
-    if (requestBody.jwt) {
-      delete requestBody.jwt;
-    }
 
-    if (typeof req.user.id !== 'undefined') {
-      requestBody.userID = req.user.id;
-    }
-  }
-  
+		if (typeof req.user.id !== 'undefined') {
+			requestBody.userID = req.user.id;
+		}
+	}
+
 	console.log(`Destination: ${req.originalUrl}`);
 	console.log(`Request Body: ${JSON.stringify(requestBody)}`);
 	console.log("Requset Suscessful");
