97a2715548a54100b3e92b58183c127891b72123
XxEngAbdullahxX <119155861+XxEngAbdullahxX@users.noreply.github.com>
2024-07-07 02:13:56 +0300

    Revert "every DANGEROUS commit added another database using prisma (still in testing)"
    This reverts commit f841fd1d613053d0381395f4eb934aa9d342bc12.

--- controllers/logController.js
+++ controllers/logController.js
@@ -1,73 +1,21 @@
-const { PrismaClient } = require('../prisma/generate/client2');
-const logPrisma = new PrismaClient({
-  datasources: {
-    db: {
-      url: process.env.LOG_DATABASE_URL,
-    },
-  },
-});
+// for log Server
 
 async function logServer(req, res) {
-  const requestBody = { ...req.body };
-  if (requestBody.jwt) {
-    delete requestBody.jwt;
-  }
-  let userId;
-  if (req.user) {
-    if (typeof req.user.id !== 'undefined') {
-      userId = req.user.id;
-    }
-  }
+	const requestBody = { ...req.body };
+	if (requestBody.jwt) {
+		delete requestBody.jwt;
+	}
+	if (req.user) {
 
-  const logData = {
-    endpoint: req.originalUrl,
-    body: JSON.stringify(requestBody),
-    status: 1,
-  };
+		if (typeof req.user.id !== 'undefined') {
+			requestBody.userID = req.user.id;
+		}
+	}
 
-  if (userId) {
-    logData.userId = userId;
-  }
-  console.log(logData);
-
-  try {
-    await logPrisma.log.create({ data: logData });
-    console.log('Log entry created successfully');
-  } catch (error) {
-    console.error('Error creating log entry:', error);
-  }
+	console.log(`Destination: ${req.originalUrl}`);
+	console.log(`Request Body: ${JSON.stringify(requestBody)}`);
+	console.log("Requset Suscessful");
+	return;
 }
 
-async function logServerError(req, res, message) {
-  const requestBody = { ...req.body };
-  if (requestBody.jwt) {
-    delete requestBody.jwt;
-  }
-  let userId;
-  if (req.user) {
-    if (typeof req.user.id !== 'undefined') {
-      userId = req.user.id;
-    }
-  }
-
-  const logData = {
-    endpoint: req.originalUrl,
-    body: JSON.stringify(requestBody),
-    status: 0,
-    message
-  };
-
-  if (userId) {
-    logData.userId = userId;
-  }
-  console.log(logData);
-
-  try {
-    await logPrisma.log.create({ data: logData });
-    console.log('Log entry created successfully');
-  } catch (error) {
-    console.error('Error creating log entry:', error);
-  }
-}
-
-module.exports = { logServer, logServerError };
+module.exports = { logServer };
